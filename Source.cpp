b4d9b2d3e2bda84b8ac95c5c51c2107ead1e6636
DadWhoCodes <87879964+TechSoCool@users.noreply.github.com>
2022-05-17 20:42:22 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+//Write a program using exception handling 
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void validate(const string& binaryString) {
+	for (unsigned i = 0; i < binaryString.size(); i++)
+	{
+		if (binaryString[i] != '0' && binaryString[i] != '1')
+		{
+			if(binaryString)
+		}
+	}
+}
+
+int main() {
+
+
+	return 0;
+}
